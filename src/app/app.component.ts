5592abfd8d2717fd168bba38603218281ec479c5
CzarEc <czar.echavez@yahoo.co.uk>
2019-11-01 00:43:31 +0000

    UPG-2: animated down button + some link icon buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,9 +25,30 @@ export class AppComponent {
     this.overlay = this.overlayContainer.getContainerElement();
     this.theme.init(this.overlay);
 
+    // icons
     this.iconReg.addSvgIcon(
       'crescent',
       sanitise.bypassSecurityTrustResourceUrl('assets/img/crescent.svg')
+    );
+    this.iconReg.addSvgIcon(
+      'cv',
+      sanitise.bypassSecurityTrustResourceUrl('assets/img/cv.svg')
+    );
+    this.iconReg.addSvgIcon(
+      'github',
+      sanitise.bypassSecurityTrustResourceUrl('assets/img/github.svg')
+    );
+    this.iconReg.addSvgIcon(
+      'linkedin',
+      sanitise.bypassSecurityTrustResourceUrl('assets/img/linkedin.svg')
+    );
+    this.iconReg.addSvgIcon(
+      'sun',
+      sanitise.bypassSecurityTrustResourceUrl('assets/img/sun.svg')
+    );
+    this.iconReg.addSvgIcon(
+      'moon',
+      sanitise.bypassSecurityTrustResourceUrl('assets/img/moon.svg')
     );
   }
 
--- src/app/components/splash-page/splash-page.component.ts
+++ src/app/components/splash-page/splash-page.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { ThemeService } from 'src/app/services/theme.service';
+import anime from 'animejs/lib/anime.es.js';
 
 @Component({
   selector: 'app-splash-page',
   templateUrl: './splash-page.component.html',
   styleUrls: ['./splash-page.component.scss']
 })
-export class SplashPageComponent implements OnInit {
+export class SplashPageComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  public seeMoreAnimation: any;
+
+  constructor(
+    public themeService: ThemeService
+    ) { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    this.seeMoreAnimation = anime({
+      targets: '.see-more',
+      translateY: 30,
+      loop: true,
+      direction: 'alternate',
+      autoplay: true,
+      duration: 1000,
+      easing: 'easeInQuad'
+    });
   }
 
   /**
@@ -18,5 +36,12 @@ export class SplashPageComponent implements OnInit {
   public scroll(id: string) {
     const el = document.getElementById(id);
     el.scrollIntoView({behavior: 'smooth'});
+  }
+
+  /**
+   * Function that toggles dark mode on the app
+   */
+  public toggleDarkMode() {
+    this.themeService.toggleDarkMode();
   }
 }
