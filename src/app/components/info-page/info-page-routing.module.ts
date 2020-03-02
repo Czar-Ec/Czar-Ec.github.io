f9e56b7b06bcbc9741bb2d5b950853ac06c7583a
CzarEc <czar.echavez@yahoo.co.uk>
2020-03-02 21:53:24 +0000

    UPG-10: Fixed scrolling

--- src/app/components/info-page/info-page-routing.module.ts
+++ src/app/components/info-page/info-page-routing.module.ts
@@ -12,11 +12,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, {
-      scrollPositionRestoration: 'enabled',
-      anchorScrolling: 'enabled',
-      // enableTracing: true
-    })
+    RouterModule.forRoot(routes)
   ],
   exports: [RouterModule]
 })
