dcf95fa58b706030187f54c0d2f6b5fb1bf089ad
CzarEc <czar.echavez@yahoo.co.uk>
2020-03-14 15:09:14 +0000

    UPG-12: retries added

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,6 +1,7 @@
 const webpack = require("@cypress/webpack-preprocessor");
 
 module.exports = on => {
+  require('cypress-plugin-retries/lib/plugin')(on)
   const options = {
     webpackOptions: require("./cy-ts-preprocessor")
   };
