d527c798dd867fb53ca2e94ce3f36d48dcd16b33
czar.echavez <czar.echavez@yahoo.co.uk>
2023-10-28 22:43:59 +0100

    136: migrate to angular 12

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './components/info-page/info-page.module#InfoPageModule' },
+  { path: '', loadChildren: () => import('./components/info-page/info-page.module').then(m => m.InfoPageModule) },
   { path: '**', redirectTo: '' }
 ];
 
