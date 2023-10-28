9474708d156e3b16ed2358a355fa0f67f2534ec2
czar.echavez <czar.echavez@yahoo.co.uk>
2023-10-28 22:40:46 +0100

    136: migrate to angular 11

--- src/app/components/info-page/info-components/info-page-navigation/info-page-navigation.component.spec.ts
+++ src/app/components/info-page/info-components/info-page-navigation/info-page-navigation.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { InfoPageNavigationComponent } from './info-page-navigation.component';
 import { SharedModule } from 'src/app/modules/shared.module';
@@ -14,7 +14,7 @@ describe('InfoPageNavigationComponent', () => {
     openBugReportPage: () => { }
   };
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       providers: [
         { provide: ExternalLinkService, useValue: stubExternalLinkService }
