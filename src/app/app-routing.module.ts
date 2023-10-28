850cb0fc7c75ba58a8ff786e37d7f25338c74b0a
czar.echavez <czar.echavez@yahoo.co.uk>
2023-10-28 23:05:43 +0100

    136: migrate to angular material 15

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { useHash: true, relativeLinkResolution: 'legacy' })
+    RouterModule.forRoot(routes, { useHash: true })
   ],
   exports: [RouterModule]
 })
