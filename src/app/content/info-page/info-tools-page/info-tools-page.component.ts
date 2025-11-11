36d6284c779fb4c2f03a9d1acba74dda9d2688fd
Czar Echavez <czar.echavez@yahoo.co.uk>
2025-11-11 03:25:57 +0000

    #180: tools list

--- src/app/content/info-page/info-tools-page/info-tools-page.component.ts
+++ src/app/content/info-page/info-tools-page/info-tools-page.component.ts
@@ -1,21 +1,19 @@
 import { Component } from '@angular/core';
 import { InfoPanelNavigationComponent } from '../info-panel-navigation/info-panel-navigation.component';
-import { InfoToolsService } from './info-tools.service';
 import { InfoToolsFilter } from './info-tools-filter/info-tools-filter.component';
+import { InfoToolsList } from './info-tools-list/info-tools-list.component';
 
 @Component({
   selector: 'app-info-tools-page',
   imports: [
-    InfoPanelNavigationComponent,
-    InfoToolsFilter
+    InfoToolsFilter,
+    InfoToolsList
   ],
   templateUrl: './info-tools-page.component.html',
   styleUrl: './info-tools-page.component.scss',
 })
 export class InfoToolsPage {
-  constructor(
-    private readonly infoToolsService: InfoToolsService
-  ) {}
+  constructor() {}
 
   
 }
